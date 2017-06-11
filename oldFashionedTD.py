a26d51efc01c91f6d44a911c6d86152e92ec94ee
Zachary Glass <zjglass@gmail.com>
2017-06-11 01:03:03 -0400

    beginning work on screen manager

--- oldFashionedTD.py
+++ oldFashionedTD.py
@@ -1,11 +1,27 @@
-import kivy
-
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class menuScreen(Screen):
+	pass
+
+class settingsScreen(Screen):
+	pass
+
+#class creditsScreen(Screen):
+#	pass
+
+#class gameScreen(Screen):
+#	pass
+
+sm = ScreenManager()
+ms = menuScreen(name = 'menu')
+ss = settingsScreen(name = 'credits')
+sm.add_widget(ms)
+sm.add_widget(ss)
 
 class oldFashionedTDApp(App):
 	def build(self):
-		return Label(text = 'oldFashioned.td')
+		return sm
 
 if __name__ == '__main__':
 	oldFashionedTDApp().run()
